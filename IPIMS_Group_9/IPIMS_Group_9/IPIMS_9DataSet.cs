46c699fea9169915c6e456b9d7b7d75f00a1dade
francescajohnson <fjohnso3@asu.edu>
2015-10-29 09:50:44 -0700

    fj

---
+++ IPIMS_Group_9/IPIMS_Group_9/IPIMS_9DataSet.cs
@@ -0,0 +1,17 @@
+﻿namespace IPIMS_Group_9 {
+    
+    
+    public partial class IPIMS_9DataSet {
+        partial class scheduled_appointment_dataDataTable
+        {
+        }
+    
+        partial class update_healthcare_dataDataTable
+        {
+        }
+    
+        partial class user_dataDataTable
+        {
+        }
+    }
+}
